97236a89dc6a02aeb80b800383cfe21df7121470
SamuelBelo7 <samuel.belo@seteloc.com.br>
2023-03-12 19:25:27 -0300

    logout button added

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,6 +1,9 @@
+import { useAuthContext } from "@/contexts/AuthContext";
 import Link from "next/link";
 
 export default function Header() {
+    const { logOut } = useAuthContext()
+
     return (
         <div className="flex items-center gap-5 justify-end text-gray-700">
             <Link href="/notes">
@@ -9,9 +12,7 @@ export default function Header() {
             {/* <Link href="/">
                 <h3>Profile</h3>
             </Link> */}
-            <Link href="/">
-                <h3>Logout</h3>
-            </Link>
+            <button onClick={() => logOut()}>Logout</button>
         </div>
     )
 }
