42810d4be3308375d7dcc9eb43fe1f65885847b4
SamuelBelo7 <samuel.belo@seteloc.com.br>
2023-04-23 19:52:52 -0300

    started update user feature

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,8 +1,9 @@
 import { useCookies } from "react-cookie";
 import jwtDecode from "jwt-decode";
-import { FormEvent, useEffect, useState } from "react";
+import { FormEvent, MouseEvent, useEffect, useState } from "react";
 import Layout from "@/components/Layout";
 import { ToastContainer, toast } from "react-toastify";
+import { gql, useMutation } from "@apollo/client";
 
 type LoggedUser = {
     id: string
@@ -12,6 +13,15 @@ type LoggedUser = {
     exp: number
 }
 
+const PUBLIC_EDIT_USER = gql`
+    mutation PublicUpdateUser($updateUserInput: UpdateUserInput!) {
+    publicUpdateUser(updateUserInput: $updateUserInput) {
+        name,
+        email
+    }
+}
+`
+
 export default function Profile() {
     const [cookie] = useCookies(["jwt"]);
     const [loggedUser, setLoggedUser] = useState<LoggedUser>()
@@ -19,6 +29,7 @@ export default function Profile() {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [repeatPassword, setRepeatPassword] = useState('')
+    const [publicUpdateUser] = useMutation(PUBLIC_EDIT_USER)
 
     useEffect(() => {
         if (cookie.jwt) {
@@ -26,9 +37,9 @@ export default function Profile() {
         }
     }, [])
 
-    function checkPassword(e: FormEvent<HTMLFormElement>) {
+    function checkPassword(e: any) {
         if (password != repeatPassword) {
-            toast.error("passwords don't match", {
+            toast.error("Passwords don't match", {
                 position: "top-right",
                 autoClose: 5000,
                 hideProgressBar: false,
@@ -39,8 +50,42 @@ export default function Profile() {
                 theme: "light",
             })
             e.preventDefault()
-        } 
+            return
+        }
+    }
 
+    async function handlePublicUpdateUser(e: any) {
+        if (password && repeatPassword) {
+            if (password != repeatPassword) {
+                toast.error("Passwords don't match", {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                })
+                e.preventDefault()
+                return
+            }
+        }
+
+        await publicUpdateUser({
+            variables: {
+                "updateUserInput": {
+                    "name": userName,
+                    "email": email,
+                    "password": password
+                }
+            },
+            context: {
+                headers: {
+                    Authorization: `Bearer ${cookie.jwt}`
+                }
+            },
+        })
     }
 
     return (
@@ -88,7 +133,7 @@ export default function Profile() {
                         />
                     </label>
 
-                    <button className='text-white bg-gray-700 mt-4 rounded-md hover:bg-gray-300 hover:text-gray-700 border border-gray-700' type='button' onClick={(e) => checkPassword(e)}>Update</button>
+                    <button className='text-white bg-gray-700 mt-4 rounded-md hover:bg-gray-300 hover:text-gray-700 border border-gray-700' type='button' onClick={(e) => handlePublicUpdateUser(e)}>Update</button>
                 </form>
             </div>
         </Layout>
